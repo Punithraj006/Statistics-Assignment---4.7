40bf35f9b55933aff72201ded91d379b3a3d15a0
Punithraj006 <39230591+Punithraj006@users.noreply.github.com>
2018-06-13 12:00:47 +0530

    Statistics Assignment - 4.7

---
+++ Assignment_4.7.py
@@ -0,0 +1,31 @@
+Problem Statement: 4.7
+
+Blood glucose levels for obese patients have a mean of 100 with a standard deviation of
+15. A researcher thinks that a diet high in raw cornstarch will have a positive effect on
+blood glucose levels. A sample of 36 patients who have tried the raw cornstarch diet
+have a mean glucose level of 108. Test the hypothesis that the raw cornstarch had an
+effect or not.
+
+Solution:
+
+Steps are as follows:-
+
+Step-1: State the hypotheses. The population mean is 100.
+
+Null Hypothesis - H0:mu = 100
+Alternate Hypothesis - H1:mu > 100
+
+Step-2: Set up the significance level. It is not given in the problem so let’s assume it as 5% (0.05).
+
+Step-3: Compute the random chance probability using z score and z-table.
+
+
+For this set of data: z= (108-100) / (15/√36)=3.20
+
+You can look at the probability by looking at z- table and p-value associated with 3.20 is 0.9993 
+
+i.e. probability of having value less than 108 is 0.9993
+`````More than or equals to 108 is (1-0.9993)=0.0007.
+
+Step-4: It is less than 0.05 so we will reject the Null hypothesis 
+            Which means, there is raw cornstarch effect.
